be95218eff367ef9fa362fc00c3eeaca601c8189
prateekwow <prateek@wowevents.in>
2022-05-09 18:22:28 +0530

    usestate

--- Reactlrn/lrnapps/src/component/basics/Menucard.js
+++ Reactlrn/lrnapps/src/component/basics/Menucard.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const Menucard = (props) => {
   return (
-      {menuData.map(curelem)}
-    <div className="card-container">
-      <div className="card">
-        <div className="card-body">
-          <span className="card-number card-circle subtle">1</span>
-          <span className='card-author subtle'>Breakfast</span>
-          <h2 className='card-title'>Joota with {props.menuData}</h2>
-          <span className='card-description subtle'>
-            I don't Like Joota but getting that in every morning.
-          </span>
-          <div className="card-read">Read</div>
-        </div>
-        <img src="/images/streamy.png" alt="img" className='card-media'/>
-        <span className="card-tag subtle">Order Now</span>
-      </div>
-    </div>
+    <section className="main-card--cointainer">
+      {props.menuData.map((curelem)=>{
+        return(
+          <div className="card-container">
+            <div className="card">
+              <div className="card-body">
+                <span className="card-number card-circle subtle">{curelem.id}</span>
+                <span className='card-author subtle'>{curelem.category}</span>
+                <h2 className='card-title'>Joota with {curelem.name}</h2>
+                <span className='card-description subtle'>
+                  {curelem.description}
+                </span>
+                <div className="card-read">Read</div>
+              </div>
+              <img src={curelem.image} alt="img" className='card-media'/>
+              <span className="card-tag subtle">Order Now</span>
+            </div>
+          </div>
+        );
+      })}
+    </section>
+    
   )
 }
 
--- Reactlrn/lrnapps/src/component/basics/resturant.js
+++ Reactlrn/lrnapps/src/component/basics/resturant.js
@@ -2,11 +2,32 @@ import React, { useState } from 'react'
 import './style.css';
 import Menu from './menuapi.js';
 import Menucard from './Menucard';
+
+
+
 const Resturant = () => {
-  const [menuData,setMenu] = React.useState(Menu);
+  const [menuData,setMenu] = useState(Menu);
   console.log(menuData);
+  const getItems=(category)=>{
+    const itemList = Menu.filter((curelement)=>{
+      return curelement.category === category;
+    });
+    // console.log(itemList);
+    setMenu(itemList);
+  }
   return (
-    <Menucard menuData={menuData} />
+    <>
+      <nav className="navbar">
+        <div className="btn-group">
+          <button className="btn-group__item" onClick={()=>getItems("breakfast")}>breakfast</button>
+          <button className="btn-group__item" onClick={()=>getItems("lunch")}>Lunch</button>
+          <button className="btn-group__item" onClick={()=>getItems("evening")}>Evening</button>
+          <button className="btn-group__item" onClick={()=>getItems("dinner")}>Dinner</button>
+          <button className="btn-group__item" onClick={()=>setMenu(Menu)}>All</button>
+        </div>
+      </nav>
+      <Menucard menuData={menuData} />
+    </>
   );
 }
 
