7443c209c86158c4e122ab83b2d0ea3551101698
prateekwow <prateek@wowevents.in>
2022-08-23 16:14:01 +0530

    08/23/2022

--- pylrn/pattern.py
+++ pylrn/pattern.py
@@ -36,4 +36,26 @@ def invpyramid(n):
                 print('t',end='')
         print('\r')
 
-invpyramid(3)
+# invpyramid(8)
+
+def invpyramidn(n):
+    x = 2*n
+    for i in range(n) :
+        t=0
+        z+=1
+        for j in range(x) :
+            if(j<n-i-1) :
+                # t+=1
+                print(' ',end='')
+            elif(j>n+i-1) :
+                # t+=1
+                print(' ',end='')
+            # elif(j == n-1):
+            #     # z=1
+            #     print(1,end='')
+            else :
+                t+=n-z-1
+                print(t,end='')
+        print('\r')
+
+invpyramidn(3)
